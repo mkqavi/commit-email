fe33ed04e6db59af38b81aa5a1a80b66abe2bf0b
Marvin König <dev@mkqavi.com>
2020-09-02 13:59:15 +0200

    feat: Change ignore array from PathBuf to remote Url

---
+++ src/config.rs
@@ -0,0 +1,97 @@
+use super::get_global_email;
+use serde::{Deserialize, Serialize};
+use std::error;
+use std::fs;
+use std::io;
+use url::Url;
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+pub struct Config {
+    #[serde(default)]
+    ignore: Vec<Url>,
+    #[serde(default)]
+    emails: Vec<String>,
+}
+
+impl Config {
+    pub fn load() -> Result<Config, Box<dyn error::Error>> {
+        let mut config_dir = match dirs::config_dir() {
+            Some(path) => path,
+            None => {
+                return Err(Box::new(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "Could not find the config directory",
+                )))
+            }
+        };
+
+        config_dir.push("commit-email");
+
+        if fs::read_dir(&config_dir).is_err() {
+            fs::create_dir(&config_dir)?;
+        }
+
+        let mut config_file = config_dir;
+        config_file.push("commit-email.toml");
+
+        let config_string = match fs::read_to_string(&config_file) {
+            Err(_) => {
+                fs::File::create(&config_file)?;
+                "".to_string()
+            }
+            Ok(content) => content,
+        };
+
+        let config: Config = match toml::from_str(&config_string) {
+            Ok(config) => config,
+            Err(_) => Config::default(),
+        };
+
+        Ok(config)
+    }
+
+    pub fn add_ignore_url(&mut self, url: &Url) {
+        if !self.ignores(&url) {
+            self.ignore.push(url.clone());
+        }
+    }
+
+    pub fn add_email(&mut self, email: &str) {
+        let email_string = email.to_string();
+        if !self.emails.contains(&email_string) {
+            self.emails.push(email_string);
+        }
+    }
+
+    pub fn get_emails(&self) -> Vec<(String, Option<String>)> {
+        let global_email = match get_global_email() {
+            Ok(email) => email,
+            Err(error) => panic!("{}", error),
+        };
+
+        let mut email_tuple = vec![(format!("Always use Global <{}>", &global_email), None)];
+        email_tuple.append(
+            &mut self
+                .emails
+                .iter()
+                .map(|email| (email.clone(), Some(email.clone())))
+                .collect(),
+        );
+
+        email_tuple
+    }
+
+    pub fn ignores(&self, url: &Url) -> bool {
+        self.ignore.contains(&url)
+    }
+
+    pub fn save(&self) -> Result<(), Box<dyn error::Error>> {
+        let mut config_path = dirs::config_dir().unwrap();
+
+        config_path.push("commit-email/commit-email.toml");
+
+        fs::write(config_path, toml::to_string_pretty(self)?)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,24 @@
+use config::Config;
 use cursive::views::{Dialog, SelectView};
 use cursive::Cursive;
 use git2::{ConfigLevel, Repository};
-use serde::{Deserialize, Serialize};
+use repo_cli::ScpPath;
 use std::env;
 use std::error;
-use std::fs;
-use std::io;
 use std::path::PathBuf;
+use url::Url;
 
-#[derive(Serialize, Deserialize, Debug, Default, Clone)]
-struct Config {
-    #[serde(default)]
-    ignore: Vec<PathBuf>,
-    #[serde(default)]
-    emails: Vec<String>,
-}
-
-impl Config {
-    fn load() -> Result<Config, Box<dyn error::Error>> {
-        let mut config_dir = match dirs::config_dir() {
-            Some(path) => path,
-            None => {
-                return Err(Box::new(io::Error::new(
-                    io::ErrorKind::NotFound,
-                    "Could not find the config directory",
-                )))
-            }
-        };
-
-        config_dir.push("commit-email");
-
-        if fs::read_dir(&config_dir).is_err() {
-            fs::create_dir(&config_dir)?;
-        }
-
-        let mut config_file = config_dir;
-        config_file.push("commit-email.toml");
-
-        let config_string = match fs::read_to_string(&config_file) {
-            Err(_) => {
-                fs::File::create(&config_file)?;
-                "".to_string()
-            }
-            Ok(content) => content,
-        };
-
-        let config: Config = match toml::from_str(&config_string) {
-            Ok(config) => config,
-            Err(_) => Config::default(),
-        };
-
-        Ok(config)
-    }
-
-    fn add_path(&mut self, path: &PathBuf) {
-        if !self.ignores(&path) {
-            self.ignore.push(path.clone());
-        }
-    }
-
-    fn add_email(&mut self, email: &str) {
-        let email_string = email.to_string();
-        if !self.emails.contains(&email_string) {
-            self.emails.push(email_string);
-        }
-    }
-
-    fn get_emails(&self) -> Vec<(String, Option<String>)> {
-        let global_email = match get_global_email() {
-            Ok(email) => email,
-            Err(error) => panic!("{}", error),
-        };
-
-        let mut email_tuple = vec![(format!("Always use Global <{}>", &global_email), None)];
-        email_tuple.append(
-            &mut self
-                .emails
-                .iter()
-                .map(|email| (email.clone(), Some(email.clone())))
-                .collect(),
-        );
-
-        email_tuple
-    }
-
-    fn ignores(&self, path: &PathBuf) -> bool {
-        self.ignore.contains(&path)
-    }
-
-    fn save(&self) -> Result<(), Box<dyn error::Error>> {
-        let mut config_path = dirs::config_dir().unwrap();
-
-        config_path.push("commit-email/commit-email.toml");
-
-        fs::write(config_path, toml::to_string_pretty(self)?)?;
-
-        Ok(())
-    }
-}
+mod config;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let config = Config::load()?;
     let current_dir = env::current_dir()?;
+    let config_url = get_repository_url(&current_dir);
 
-    if config.ignores(&current_dir) {
-        return Ok(());
+    if let Some(url) = &config_url {
+        if config.ignores(&url) {
+            return Ok(());
+        }
     }
 
     if get_repository_email(&current_dir)?.is_some() {
@@ -118,7 +32,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let mut sv = SelectView::new();
     sv.add_all(config.get_emails());
     sv.set_on_submit(move |ui, email| {
-        submit_email(ui, email, &mut config.clone(), current_dir.clone())
+        submit_email(ui, email, &mut config.clone(), config_url.clone())
     });
 
     ui.add_layer(Dialog::around(sv).title("Please select an email for your commit"));
@@ -144,6 +58,25 @@ fn get_repository_email(current_dir: &PathBuf) -> Result<Option<String>, Box<dyn
     }
 }
 
+fn get_repository_url(current_dir: &PathBuf) -> Option<Url> {
+    let repo = Repository::open(current_dir).ok()?;
+
+    let remote = match repo.find_remote("origin") {
+        Ok(remote) => remote,
+        Err(_) => match repo.remotes().ok()?.get(0) {
+            Some(remote_name) => repo.find_remote(remote_name).ok()?,
+            None => return None,
+        },
+    };
+
+    let url_string = remote.url()?;
+
+    match ScpPath::parse(url_string) {
+        Ok(scp_path) => Some(scp_path.to_url()),
+        Err(_) => Url::parse(url_string).ok(),
+    }
+}
+
 fn get_global_email() -> Result<String, Box<dyn error::Error>> {
     let config = git2::Config::open_default()?;
 
@@ -154,7 +87,7 @@ fn submit_email(
     ui: &mut Cursive,
     email: &Option<String>,
     config: &mut Config,
-    current_dir: PathBuf,
+    config_url: Option<Url>,
 ) {
     ui.quit();
 
@@ -165,7 +98,11 @@ fn submit_email(
             // Write to .git/config
             println!("Writing {} to .git/config", email);
         }
-        None => config.add_path(&current_dir),
+        None => {
+            if let Some(url) = &config_url {
+                config.add_ignore_url(&url);
+            }
+        }
     }
 
     config.save().unwrap();
